5f31660422ef866ebafc89da6695076505dc31f9
unknown <SENA@SGLDFPCATSD262.FormacionSena.RED>
2023-10-03 13:53:23 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import "dotenv/config"
+import mongoose from "mongoose";
+
+
+const index = express()
+index.use(express.json())
+
+index.use("/api/area",)
+index.use("/api/lote",)
+index.use("/api/usuarios",)
+index.use("/api/inventario",)
+index.use("/api/pedido",)
+index.use("/api/ficha",)
+
+index.listen(process.env.PORT, () => {
+    console.log(
+        `Servidor escuchando en el puerto ${process.env.PORT}`
+    );
+    mongoose.connect(process.env.mongoDB)
+        .then(() => console.log(`Conexion Activa`));
+})
