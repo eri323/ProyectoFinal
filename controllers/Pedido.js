ee0892539afc3825b0b24ff935e7d5f704bcf16f
unknown <SENA@SGLDFPCATSD262.FormacionSena.RED>
2023-10-05 14:39:48 -0500

    eri 2

---
+++ controllers/Pedido.js
@@ -0,0 +1,69 @@
+import Pedidos from "../models/Pedidos.js";
+
+const httpPedidos = {
+    getPedidos: async (req, res) => {
+        try {
+            const pedidos = await Pedidos.find();
+            res.json({ pedidos });
+        } catch (error) {
+            res.status(400).json({ error });
+        }
+    },
+    getPedidosId: async (req, res) => {
+        const { id } = req.params;
+        try {
+            const pedidos = await Pedidos.findById(id);
+            res.json({ pedidos });
+        } catch (error) {
+            res.status(400).json({ error });
+        }
+    },
+
+    postPedidos: async (req, res) => {
+        try {
+            const { FechaCreacion, FechaEntrega, IdDistribucionLoteFicha, Subtotal, Total} = req.body;
+            const pedidos = new Pedidos({ FechaCreacion, FechaEntrega, IdDistribucionLoteFicha, Subtotal, Total});
+            pedidos.save();
+            res.json({ pedidos });
+        } catch (error) {
+            res.status(400).json({ error });
+        }
+
+    },
+
+    putPedidos: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const { FechaCreacion, FechaEntrega, IdDistribucionLoteFicha, Subtotal, Total} = req.body;
+            const pedidos = await Pedidos.findByIdAndUpdate(id, { FechaCreacion, FechaEntrega, IdDistribucionLoteFicha, Subtotal, Total}, { new: true });
+            res.json({ pedidos });
+        } catch (error) {
+            res.status(400).json({ error: "Error en el servidor" });
+        }
+    },
+
+
+    putPedidosInactivar: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const pedidos = await Pedidos.findByIdAndUpdate(id, { estado: 0 }, { new: true });
+            res.json({ pedidos });
+        } catch (error) {
+            res.status(400).json({ error });
+        }
+    },
+
+
+    putPedidosActivar: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const pedidos = await Pedidos.findByIdAndUpdate(id, { estado: 1 }, { new: true });
+            res.json({ pedidos });
+        } catch (error) {
+            res.status(400).json({ error });
+        }
+    }
+
+};
+
+export default httpPedidos;
